002f83e715f4cf2738d44c9026942399c9c2d7bc
bulle <percevallechat@gmail.com>
2023-01-15 18:09:04 +0100

    hello

---
+++ backend/server.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	// "github.com/mattn/go-sqlite3"
+
+	"time"
+	// "github.com/gorilla/sessions"
+)
+
+// var database =
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func signinHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/signinconf" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	name := r.FormValue("name")
+	password := r.FormValue("password")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "password = %s\n", password)
+	fmt.Fprintf(w, "email = %s\n", email)
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("../front")) // New code
+	http.Handle("/", fileServer)                        // New code
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/signinconf", signinHandler)
+	// http.HandleFunc("/signupconf", signupHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkErr(err)
+
+	// insert
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+	// update
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("astaxieupdate", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	// query
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+	var uid int
+	var username string
+	var department string
+	var created time.Time
+
+	for rows.Next() {
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
+	}
+
+	rows.Close() //good habit to close
+
+	// delete
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+}
